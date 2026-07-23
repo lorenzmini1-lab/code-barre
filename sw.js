39fe744a366013680c18535ded40dee564720a6c
lorenzmini1-lab <lorenzmini1@gmail.com>
2026-07-23 10:22:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barcode-pwa-v1';
+const CACHE_NAME = 'barcode-pwa-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -10,6 +10,20 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
